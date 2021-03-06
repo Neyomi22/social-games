21fceef40e032035a56e4d365b3f6c450bdf3c74
iantaylor <iantaylor03@gmail.com>
2021-03-06 18:22:51 +1100

    adding events controller to main branch

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,38 +1,39 @@
 class EventsController < ApplicationController
 
   def index
-    if params[:location].nil?
-      @events = Event.all
-      @users = User.all
-    else
-      @events = []
-      @user.near("#{params[:location]},"params[:location].to_i order: :distance)
-      @users.each do |user|
-        @events << Event.where(user_id: user.id)
+      if params[:location].nil?
+        @events = Event.all
+        @users = User.all
+      else
+        @events = []
+        @user.near("#{params[:location]}", params[:location].to_i, order: :distance)
+        @users.each do |user|
+          @events << Event.where(user_id: user.id)
+        end
+        @events.flatten!
       end
-      @events.flatten!
-    end
     # the `geocoded` scope filters only events with coordinates (latitude & longitude)
-    @markers = @eventss.geocoded.map do |event|
-      {
-        lat: event.latitude,
-        lng: event.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { event: event })
-      }
-    end
+      @markers = @events.geocoded.map do |event|
+        {
+          lat: event.latitude,
+          lng: event.longitude,
+          infoWindow: render_to_string(partial: "info_window", locals: { event: event })
+        }
+      end
   end
 
   def create
     @event = Event.new(event_params)
     @event.user_id = current_user.id
     if @event.save
-      redirect_to events_show_path
+      redirect_to event_path(@event)
     else
-      render :new
+      render :new   
+    end
   end
 
   def new
-    @events = Event.new()
+    @event = Event.new
   end
 
   def edit
@@ -56,16 +57,12 @@ class EventsController < ApplicationController
     else
       render event_path
     end
+  end
 
   private
 
   def event_params
-    params.require(:event).permit(:title, :location, :time, :date, :sport, :number_of_participants, :description, :private)
-    
+    params.require(:event).permit(:title, :location, :time, :date, :sport, :number_of_participants, :description, :private, :duration)
   end
-  
-  
-  
-  
-  
+
 end
