00794990095f8220305248010ea2b51e8d01e202
Amanda Paranamana <neyomi22@hotmail.com>
2021-03-07 21:07:30 +1100

    Added event model

---
+++ db/migrate/20210307100618_create_events.rb
@@ -0,0 +1,18 @@
+class CreateEvents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :events do |t|
+      t.string :location
+      t.date :date
+      t.time :time
+      t.string :sport
+      t.integer :number_of_participants
+      t.text :description
+      t.string :title
+      t.integer :duration
+      t.string :private
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_094643) do
+ActiveRecord::Schema.define(version: 2021_03_07_100618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "location"
+    t.date "date"
+    t.time "time"
+    t.string "sport"
+    t.integer "number_of_participants"
+    t.text "description"
+    t.string "title"
+    t.integer "duration"
+    t.string "private"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +48,5 @@ ActiveRecord::Schema.define(version: 2021_03_07_094643) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
