0153074e4d6b5a4bc6cbd1a2bf1453ded8cfbc91
Amanda Paranamana <neyomi22@hotmail.com>
2021-03-07 22:12:27 +1100

    Added validations to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,17 @@
 class Event < ApplicationRecord
+  #association
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+
+  # event picture
+  has_one_attached :photo
+  
+  # validations
+  validates :user_id, presence: true
+  validates :title, presence: true
+  validates :date, presence: true
+  validates :time, presence: true
+  validates :location, presence: true
+  validates :sport, presence:true
+  validates :duration, presence: true
 end
