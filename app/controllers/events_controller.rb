294896e3b4e4b81363578a0f375c6e026e45ad75
Amanda Paranamana <neyomi22@hotmail.com>
2021-03-08 08:01:53 +1100

    Geocoder setup completed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,15 @@
 class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+
+    # the `geocoded` scope filters only events with coordinates (latitude & longitude)
+    @markers = @eventss.geocoded.map do |event|
+      {
+        lat: event.latitude,
+        lng: event.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { event: event })
+      }
+    end
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,10 @@ class Event < ApplicationRecord
   # event picture
   has_one_attached :photo
   
+  #longitude and latitude
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+  
   # validations
   validates :user_id, presence: true
   validates :title, presence: true
