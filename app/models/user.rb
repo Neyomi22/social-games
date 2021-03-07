205f5c7c7cf6ecce8232a6ffa2cb1deb24411cd0
Amanda Paranamana <neyomi22@hotmail.com>
2021-03-07 22:07:22 +1100

    Added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+   # profile picture
+   has_one_attached :photo
+   
+   # association
+   has_many :events, dependent: :destroy
+   has_many :bookings, through: :events
 end
