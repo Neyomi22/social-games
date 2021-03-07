ef44551d365f6529f3a2aa5a9400be2e2b1b5a1a
Amanda Paranamana <neyomi22@hotmail.com>
2021-03-07 22:14:32 +1100

    Added validations to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
+  #association
   belongs_to :user
   belongs_to :event
+  
+  # validations
+  validates :user_id, presence: true
+  validates :event_id, presence: true, uniqueness: {scope: :user_id}
+
 end
