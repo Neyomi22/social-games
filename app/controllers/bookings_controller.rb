d6511e0ad37b94583bf575b78b895be987271ef3
Amanda Paranamana <neyomi22@hotmail.com>
2021-03-08 11:53:14 +1100

    Created index view page for bookings and index,new,create methods in booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,32 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:create, :new]
+
+  def index
+    @bookings = Booking.where(user_id: current_user.id)
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.event = @event
+    @booking.user = current_user
+    if @booking.save
+      redirect_to bookings_path
+    else 
+      render :new
+    end
+  end 
+
+  private
+
+  def set_booking
+    @event = Event.find(params[:event_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :event_id)
+  end
 end
