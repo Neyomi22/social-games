64f8e71ec322b1417089e509ed8bc546be9b704a
Amanda Paranamana <neyomi22@hotmail.com>
2021-03-08 16:50:52 +1100

    Added the new migration file

--- db/migrate/20210307100618_create_events.rb
+++ db/migrate/20210307100618_create_events.rb
@@ -2,8 +2,7 @@ class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
       t.string :location
-      t.date :date
-      t.time :time
+      t.datetime :starts_at
       t.string :sport
       t.integer :number_of_participants
       t.text :description
