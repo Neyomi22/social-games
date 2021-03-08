66bd4d74df6d76bc21f94ab0725cc3693abf1c30
akash chhetri <akash.chhetri1@gmail.com>
2021-03-08 11:26:28 +1100

    db seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Users
+users = %w(akash@gmail.com amanda@gmail.com ian@gmail.com)
+password = '123456'
+sports = %w(Footy Soccer Badminton Tennis Chess)
+location = %w(Officer Watsonia Clyde)
+users.each do |user|
+  User.create(email: user, password: password)
+end
+
+# Events
+
+20.times do 
+  hash = {
+    date: Faker::Date.forward(days: 10),
+    sport: sport.sample,
+    number_of_participants: rand(1..11),
+    description: Faker::Lorem.paragraph,
+    title: Faker::Music.album,
+    duration: rand(1..3),
+    private: [nil, "superman"].sample,
+    user_id: rand(1..3)
+    location: "#{location.sample}, Victoria, Australia"
+  }
+  Event.create(hash)
+end
+
+# Bookings booking 20
+20.times do
+  event = Event.find(rand(1..20))
+  user = [1,2,3].reject { |num| num == event.user_id}.sample
+  Booking.create(user_id: user.id, event_id: event.id)
+end
