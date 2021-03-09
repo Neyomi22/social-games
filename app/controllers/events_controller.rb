c68617636c1f77b8e113029d13921243879804aa
Amanda Paranamana <neyomi22@hotmail.com>
2021-03-09 14:13:06 +1100

    Display the participants to event show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -48,7 +48,7 @@ class EventsController < ApplicationController
   
   def show
     @event = Event.find(params[:id])
-    @booking = Booking.new
+    @bookings = Booking.where(event: @event)
   end
 
   def destroy
