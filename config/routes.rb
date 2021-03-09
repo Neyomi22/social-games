c33ecf33be9699110d1b1874927f1eb516ad76ae
akash chhetri <akash.chhetri1@gmail.com>
2021-03-09 11:28:16 +1100

    dashboard routes added and dashboard link added to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   end
   resources :bookings, only: [ :index, :show, :destroy, :update ]
   resources :profiles, only: [ :edit, :update ]
+  resources :dashboards, only: [ :index, :show ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
