40566a15892f22ae7a69d7882cfc3dbf1def15c5
akash chhetri <akash.chhetri1@gmail.com>
2021-03-12 10:42:39 +1100

    delete event cascade deletes chatroom and messaged

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -59,7 +59,7 @@ class EventsController < ApplicationController
   def destroy
     @event = Event.find(params[:id])
     if @event.user == current_user && @event.destroy
-      redirect_to events_path, :notice => "Your event has been deleted!"
+      redirect_to dashboards_path, :notice => "Your event has been deleted!"
     else
       render "events/show" 
     end
