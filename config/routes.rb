cc9983bb7362980d53a6a6396f612ddbbbfb1619
akash chhetri <akash.chhetri1@gmail.com>
2021-03-11 15:43:50 +1100

    bookings routes filtered

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :events do
-    resources :bookings, only: [ :new, :create]
+    resources :bookings, only: [:create]
   end
-  resources :bookings, only: [ :show, :destroy ]
+  resources :bookings, only: [:show, :destroy]
  
   resources :chatrooms, only: :show do
-    resources :messages, only: [ :create ]
+    resources :messages, only: [:create]
   end # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :bookings, only: [ :index, :show, :destroy, :update ]
-  resources :profiles, only: [ :edit, :update ]
-  resources :dashboards, only: [ :index, :show ]
+  resources :profiles, only: [:edit, :update]
+  resources :dashboards, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
