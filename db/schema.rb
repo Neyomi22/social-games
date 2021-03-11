03a74cfdb08e4dbe331f356745d6439174168505
iantaylor <iantaylor03@gmail.com>
2021-03-11 17:01:40 +1100

    hidding the mapbox search bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2021_03_11_033740) do
-=======
-ActiveRecord::Schema.define(version: 2021_03_10_004442) do
->>>>>>> 20ab0bf09dd99829bf63de0d9e86873287d69b3f
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
