f7884582218278dec3870aa7b53a73794b392488
akash chhetri <akash.chhetri1@gmail.com>
2021-03-13 09:28:57 +1100

    chat box open and close functionality added

--- app/javascript/live_chat/chat_box.js
+++ app/javascript/live_chat/chat_box.js
@@ -1,8 +1,12 @@
 const initChatBox = () => {
 	const chatBoxDom = document.querySelector('.message-icon');
 	const chatContainerDom = document.querySelector('.message-body__container');
+	const chatHeaderDom = document.querySelector('.close-message');
 	chatBoxDom.addEventListener('click', () => {
 		chatContainerDom.style.visibility = 'visible';
+	});
+	chatHeaderDom.addEventListener('click', () => {
+		chatContainerDom.style.visibility = 'hidden';
 	});
 };
 
