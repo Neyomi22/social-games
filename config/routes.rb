6b9c9c07df94accfa7aa94d1528ee72080d4726a
akash chhetri <akash.chhetri1@gmail.com>
2021-03-15 09:51:14 +1100

    dashboard show routes removed

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   end # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :profiles, only: [:edit, :update]
-  resources :dashboards, only: [:index, :show]
+  resources :dashboards, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
