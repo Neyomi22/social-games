52c1dabf9693321cb89390053e84273da5f57bd0
akash chhetri <akash.chhetri1@gmail.com>
2021-03-16 14:01:56 +1100

    toggle message box

--- app/javascript/live_chat/chat_box.js
+++ app/javascript/live_chat/chat_box.js
@@ -1,15 +1,9 @@
 const initChatBox = () => {
-	const chatBoxDom = document.querySelector('.message-icon');
-	const chatContainerDom = document.querySelector('.message-body__container');
+	const chatBoxDom = document.getElementById('messages');
 	const chatHeaderDom = document.querySelector('.close-message');
-	if (chatBoxDom && chatContainerDom) {
-		chatBoxDom.addEventListener('click', () => {
-			chatContainerDom.style.visibility = 'visible';
-		});
-		chatHeaderDom.addEventListener('click', () => {
-			chatContainerDom.style.visibility = 'hidden';
-		});
-	}
+	chatHeaderDom.addEventListener('click', () => {
+		chatBoxDom.classList.toggle('toggle-visibility-chat');
+	});
 };
 
 export { initChatBox };
