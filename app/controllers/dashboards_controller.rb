a63f846d33c2953f99c00a1e82ebeae5783208cc
akash chhetri <akash.chhetri1@gmail.com>
2021-03-17 10:30:18 +1100

    dashboard show controller added

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,14 +1,19 @@
 class DashboardsController < ApplicationController
   def index
-    @bookings = Booking.where(user: current_user)
-    @bookings = @bookings.sort_by{|b| b.created_at }.reverse
-    @events = Event.where(user: current_user)
-    @events = @events.sort_by{|e| e.starts_at }
+    user_info(true)
   end
 
   def show
-    @event = Event.find(params[:id])
-    @bookings = Booking.where(event: @event)
+    user_info(false)
   end
 
+  private
+
+  def user_info(user_exists)
+    user = user_exists ? current_user : User.find(params[:id])
+    @bookings = Booking.where(user: user)
+    @bookings = @bookings.sort_by{|b| b.created_at }.reverse
+    @events = Event.where(user: user)
+    @events = @events.sort_by{|e| e.starts_at }
+  end
 end
