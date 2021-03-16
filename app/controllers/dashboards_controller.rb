c6c55dae698be86b723cbf784e137c8cd5d7180a
akash chhetri <akash.chhetri1@gmail.com>
2021-03-17 10:41:45 +1100

    action designed for current user

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,19 +1,21 @@
 class DashboardsController < ApplicationController
   def index
-    user_info(true)
+    @owner = true
+    user_info
   end
 
   def show
-    user_info(false)
+    @owner = false
+    user_info
   end
 
   private
 
-  def user_info(user_exists)
-    user = user_exists ? current_user : User.find(params[:id])
-    @bookings = Booking.where(user: user)
+  def user_info
+    @user = @owner ? current_user : User.find(params[:id])
+    @bookings = Booking.where(user: @user)
     @bookings = @bookings.sort_by{|b| b.created_at }.reverse
-    @events = Event.where(user: user)
+    @events = Event.where(user: @user)
     @events = @events.sort_by{|e| e.starts_at }
   end
 end
