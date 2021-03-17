1c6a3c8458a30ee960f30fdaa93cdad999865ca4
akash chhetri <akash.chhetri1@gmail.com>
2021-03-17 17:32:02 +1100

    message box container moves with footer

--- app/javascript/live_chat/chat_bar_scroll.js
+++ app/javascript/live_chat/chat_bar_scroll.js
@@ -1,13 +1,28 @@
 const initChatBarScroll = () => {
 	const messageDom = document.getElementById('messages');
+	const messageContainerDom = document.querySelector(
+		'.message-body__container'
+	);
 	const footerDom = document.querySelector('.footer');
-	console.log('charbar');
-	if (messageDom && footerDom) {
-		console.log(document.scrollTop);
-		// console.log(messageDom.scrollTop);
-		//		messageDom.addEventListener('scroll', (el) => {
-		//			console.log(el.scrollLeft, el.scrollTop);
-		//		});
+	if (messageContainerDom && messageDom && footerDom) {
+		// event to prevent message box to move over footer
+		// 1. get the scrollable window height
+		const windowHeight =
+			document.documentElement.scrollHeight - window.innerHeight;
+		// 2. get footer height
+		const footerHeight = footerDom.offsetHeight;
+		// 3. get the window without footer height
+		const remainingWindow = windowHeight - footerHeight;
+		// 4. add event listener to scroll and check current scroll position
+		window.addEventListener('scroll', (e) => {
+			// 4 get the current scroll position
+			let currentScrollPostion = window.scrollY;
+			if (remainingWindow <= currentScrollPostion) {
+				messageContainerDom.style.bottom = `${
+					10 + currentScrollPostion - remainingWindow
+				}px`;
+			}
+		});
 	}
 };
 export { initChatBarScroll };
