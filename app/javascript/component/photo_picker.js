50f312d2720415ce66315dadcdfc54f35975fecb
akash chhetri <akash.chhetri1@gmail.com>
2021-03-17 11:12:05 +1100

    showpage design complete and photopicker bug fixed

--- app/javascript/component/photo_picker.js
+++ app/javascript/component/photo_picker.js
@@ -1,12 +1,15 @@
 const initPhotoPicker = () => {
 	const fileUploadEl = document.querySelector('.js-photo-input');
-	fileUploadEl.addEventListener('change', function (event) {
-		const jsPhotoPreviewEl = document.querySelector('.js-photo-preview');
-		jsPhotoPreviewEl.src = URL.createObjectURL(event.target.files[0]);
-		jsPhotoPreviewEl.onload = function () {
-			URL.revokeObjectURL(jsPhotoPreviewEl.src); // free memory
-		};
-	});
+
+	if (fileUploadEl) {
+		fileUploadEl.addEventListener('change', function (event) {
+			const jsPhotoPreviewEl = document.querySelector('.js-photo-preview');
+			jsPhotoPreviewEl.src = URL.createObjectURL(event.target.files[0]);
+			jsPhotoPreviewEl.onload = function () {
+				URL.revokeObjectURL(jsPhotoPreviewEl.src); // free memory
+			};
+		});
+	}
 };
 
 export { initPhotoPicker };
