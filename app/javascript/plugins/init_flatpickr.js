868f0c8a27dcdca4d1df43f2e7b15cf7b7184707
akash chhetri <akash.chhetri1@gmail.com>
2021-03-18 15:05:38 +1100

    date time added to the add input form

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -2,7 +2,10 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const initFlatpickr = () => {
-	flatpickr('.datepicker', {});
+	flatpickr('.datepicker', {
+		altInput: true,
+		enableTime: true,
+	});
 };
 
 export { initFlatpickr };
